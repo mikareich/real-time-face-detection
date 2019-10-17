08dc185538bc72272eb3a6fec713585fe01b97db
Judith Berg <judithberg@Judiths-MBP.fritz.box>
2019-10-17 20:14:31 +0200

    change uri

--- srcipts/app.js
+++ srcipts/app.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/real-time-face-detection/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/real-time-face-detection/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/real-time-face-detection/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/real-time-face-detection/models')
 ]).then(startVideo)
 
 function startVideo() {
